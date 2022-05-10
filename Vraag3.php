c9afa0a31c032d265ce664df8a25230df98d6538
floris456 <66360565+floris456@users.noreply.github.com>
2022-05-10 18:35:32 +0200

    Add files via upload

---
+++ Vraag3.php
@@ -0,0 +1,10 @@
+<?php
+$txt1 = "Floris Kloeg";
+$txt2 = "Lab 3 CST SSII";
+$x = 19063253;
+
+
+print "<h2>" . $txt1 . "</h2>";
+print "<p2>" . $txt2 . "</p2>";
+print "<br> Studienummer " . $x . "<br>";
+?>
